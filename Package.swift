dcc7b97a50056c203277913562fc3fb3b6960117
chenjh <c.hjong@outlook.com>
2026-04-27 23:02:38 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -17,9 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         //.binaryTarget(name: "mdk-sdk", path: "mdk-sdk/lib/mdk.xcframework"),
-        .binaryTarget(name: "mdk-sdk"
-            , url: "https://github.com/wang-bin/mdk-sdk/releases/download/v0.35.1/mdk-sdk-apple.zip"
-            , checksum: "510698c65c0851940e1b8872e7b713a0fb0f258b6266b0c4aa057e0d0523d937"),
+        .binaryTarget(name: "mdk-sdk", path: "mdk-sdk/lib/mdk.xcframework"),
         .target(
             name: "swift-mdk",
             dependencies: ["mdk-sdk"]),
